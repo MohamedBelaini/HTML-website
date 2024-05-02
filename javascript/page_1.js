6061507d59336225dad71b85b010f2c7827d3fcb
mohamed belaini <belainimohamed78@gmail.com>
2024-05-02 14:52:48 +0100

    sm

---
+++ javascript/page_1.js
@@ -0,0 +1,13 @@
+let content = document.getElementById('content')
+
+function Onclick(id) {
+   if(id == 'btn1') {
+        content.innerHTML
+   } else if (id == 'btn2') {
+        content.innerHTML
+   }else if (id == 'btn3') {
+        content.innerHTML
+   }else if (id == 'btn4') {
+        content.innerHTML
+   }
+} 
